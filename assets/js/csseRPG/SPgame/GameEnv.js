e59ae945a70946fa49ba07a0120500ff47b1de94
KKATZENN <paul.yue10@gmail.com>
2025-02-10 12:42:00 -0800

    git add

--- assets/js/csseRPG/SPgame/GameEnv.js
+++ assets/js/csseRPG/SPgame/GameEnv.js
@@ -1,3 +1,13 @@
+/**properties and stuff
+ * @class GameEnv
+ * @property {Array} gameObjects - An array of game objects for the current level.
+ * @property {Object} canvas - The canvas element.
+ * @property {Object} ctx - The 2D rendering context of the canvas.
+ * @property {number} innerWidth - The inner width of the game area.
+ * @property {number} innerHeight - The inner height of the game area.
+ * @property {number} top
+ */
+
 class GameEnv {
     static gameObjects = [];
     static canvas;
