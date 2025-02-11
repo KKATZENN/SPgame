22773c1489579f5f410d01208ad6b3f99c5d1171
KKATZENN <paul.yue10@gmail.com>
2025-02-11 11:41:28 -0800

    game.env added

--- assets/js/csseRPG/SPgame/GameEnv.js
+++ assets/js/csseRPG/SPgame/GameEnv.js
@@ -1,23 +1,23 @@
-/**properties and stuff
+/**
  * @class GameEnv
  * @property {Array} gameObjects - An array of game objects for the current level.
  * @property {Object} canvas - The canvas element.
  * @property {Object} ctx - The 2D rendering context of the canvas.
  * @property {number} innerWidth - The inner width of the game area.
  * @property {number} innerHeight - The inner height of the game area.
- * @property {number} top
- * add
+ * @property {number} top - The top offset of the game area.
+ * @property {number} bottom - The bottom offset of the game area.
  */
-
 class GameEnv {
     static gameObjects = [];
+    static continueLevel = true;
     static canvas;
     static ctx;
     static innerWidth;
     static innerHeight;
     static top;
     static bottom;
-}
+    
     /**
      * Private constructor to prevent instantiation.
      * 
@@ -27,3 +27,71 @@ class GameEnv {
     constructor() {
         throw new Error('GameEnv is a static class and cannot be instantiated.');
     }
+
+    /**
+     * @static
+     */
+    static create() {
+        this.setCanvas();
+        this.setTop();
+        this.setBottom();
+        this.innerWidth = window.innerWidth;
+        this.innerHeight = window.innerHeight - this.top - this.bottom;
+        this.size();
+    }
+
+    /**
+     * @static
+     */
+    static setCanvas() {
+        this.canvas = document.getElementById('gameCanvas');
+        this.ctx = this.canvas.getContext('2d');
+    }
+
+    /**
+     * @static
+     */
+    static setTop() {
+        const header = document.querySelector('header');
+        this.top = header ? header.offsetHeight : 0;
+    }
+
+    /**
+     * Sets the bottom offset based on the height of the footer element.
+     * 
+     * @static
+     */
+    static setBottom() {
+        const footer = document.querySelector('footer');
+        this.bottom = footer ? footer.offsetHeight : 0;
+    }
+
+    /**
+     * @static
+     */
+    static size() {
+        this.canvas.width = this.innerWidth;
+        this.canvas.height = this.innerHeight;
+        this.canvas.style.width = `${this.innerWidth}px`;
+        this.canvas.style.height = `${this.innerHeight}px`;
+        this.canvas.style.position = 'absolute';
+        this.canvas.style.left = '0px';
+        this.canvas.style.top = `${this.top}px`;
+    }
+
+    /**
+     * @static
+     */
+    static resize() {
+        this.create();
+    }
+
+    /**
+     * @static
+     */
+    static clear() {
+        this.ctx.clearRect(0, 0, this.innerWidth, this.innerHeight);
+    }
+}
+
+export default GameEnv;
