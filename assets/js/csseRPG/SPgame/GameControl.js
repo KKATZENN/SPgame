f2cac2c5bb42be69b6232e0f283ffe6c1cad2f10
KKATZENN <paul.yue10@gmail.com>
2025-02-26 05:26:10 +0000

    move + finish game control

--- assets/js/csseRPG/SPgame/GameControl.js
+++ assets/js/csseRPG/SPgame/GameControl.js
@@ -22,23 +22,6 @@ const createStatsUI = () => {
     document.body.appendChild(statsContainer);
 };
 
-/**
- * The GameControl object manages the game.
- * 
- * This code uses the JavaScript "object literal pattern" which is nice for centralizing control logic.
- * 
- * The object literal pattern is a simple way to create singleton objects in JavaScript.
- * It allows for easy grouping of related functions and properties, making the code more organized and readable.
- * In the context of GameControl, this pattern helps centralize the game's control logic, 
- * making it easier to manage game states, handle events, and maintain the overall flow of the game.
- * 
- * @type {Object}
- * @property {Player} turtle - The player object.
- * @property {Player} fish 
- * @property {function} start - Initialize game assets and start the game loop.
- * @property {function} gameLoop - The game loop.
- * @property {function} resize - Resize the canvas and player object when the window is resized.
- */
 const GameControl = {
     intervalID: null, // Variable to hold the timer interval reference
     localStorageTimeKey: "localTimes",
@@ -140,11 +123,6 @@ const GameControl = {
         });
     },
 
-    /**
-     * Updates and displays the game timer.
-     * @function updateTimer
-     * @memberof GameControl
-     */ 
     saveTime(time, score) {
         if (time == 0) return;
         const userID = GameEnv.userID
@@ -192,11 +170,7 @@ const GameControl = {
                 document.getElementById('timeScore').textContent = (time/1000).toFixed(2) 
             }
     },   
-    /**
-     * Starts the game timer.
-     * @function startTimer
-     * @memberof GameControl
-     */
+
     startTimer() {
         if (GameEnv.timerActive) {
             console.warn("TIMER ACTIVE: TRUE, TIMER NOT STARTED")
@@ -207,11 +181,6 @@ const GameControl = {
         GameEnv.timerActive = true;
     },
 
-    /**
-     * Stops the game timer.
-     * @function stopTimer
-     * @memberof GameControl
-     */
     stopTimer() {   
         if (!GameEnv.timerActive) return;
         
